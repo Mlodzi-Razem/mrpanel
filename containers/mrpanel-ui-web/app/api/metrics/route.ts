b62f209ce349feb6d762281a29f262b4bcbc88a3
Kamil Łapiński <contact@lapinski.dev>
2025-11-04 20:07:16 +0100

    Added metrics endpoint protection

--- containers/mrpanel-ui-web/app/api/metrics/route.ts
+++ containers/mrpanel-ui-web/app/api/metrics/route.ts
@@ -1,22 +1,65 @@
 import {collectDefaultMetrics, register} from "prom-client";
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
 function registerMetricsIfNeeded() {
     if (!('metricsRegistered' in globalThis)) {
         const prefix = 'mrpanel_ui_web';
         collectDefaultMetrics({prefix});
-        (globalThis as unknown as {metricsRegistered: boolean}).metricsRegistered = true;
+        (globalThis as unknown as { metricsRegistered: boolean }).metricsRegistered = true;
     }
 }
 
 registerMetricsIfNeeded();
 
-export async function GET() {
-    return new NextResponse(
-        await register.metrics(),
-        {
-            headers: {
-                'Content-Type': register.contentType
-            }
-        });
+async function createResponse() {
+    const metrics = await register.metrics();
+    const headers = {
+        'Content-Type': register.contentType
+    };
+
+    return new NextResponse(metrics, {headers});
+}
+
+const UNAUTHORIZED_BODY = JSON.stringify({message: "Unauthorized"});
+const NO_TOKEN_BODY = JSON.stringify({message: 'Metrics token not configured'});
+
+const UNAUTHORIZED = new NextResponse(UNAUTHORIZED_BODY, {status: 401});
+const NO_TOKEN = new NextResponse(NO_TOKEN_BODY, {status: 500});
+
+function validateRequest(req: NextRequest) {
+    const authHeader = req.headers.get('Authorization');
+    if (!authHeader) {
+        throw UNAUTHORIZED;
+    }
+
+    const expectedToken = process.env.METRICS_TOKEN;
+    if (!expectedToken) {
+        throw NO_TOKEN;
+    }
+
+    const tokensMatch = authHeader.trim() === `Bearer ${expectedToken}`.trim();
+    if (!tokensMatch) {
+        throw UNAUTHORIZED;
+    }
+}
+
+/**
+ * This endpoint exposes metrics for Prometheus.
+ *
+ * The endpoint is protected by a token to prevent unauthorized access.
+ * The token should be regenerated on every deployment.
+ */
+export async function GET(req: NextRequest) {
+    try {
+        validateRequest(req);
+        return await createResponse();
+    } catch (e) {
+        console.error("Error while handling metrics request", {url: req.url, headers: req.headers});
+
+        if (e instanceof NextResponse) {
+            return e;
+        }
+
+        throw e;
+    }
 }
